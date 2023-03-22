293c7a2e99dbd02cea875aff417288f79ea298fc
Azaruddin8297 <ajarpopo.com>
2023-03-22 19:45:55 +0530

    EmployeeWage

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeWage
+{
+    public class Employee
+    {
+        public static void Main(string[] args)
+        {
+            EmpWageBulidArray empWageBulidArray = new EmpWageBulidArray();
+            empWageBulidArray.addCompanyEmpWages("DMart", 20, 2, 10);
+            empWageBulidArray.computeEmpWage();
+        }
+    }
+}
