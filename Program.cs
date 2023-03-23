51a14e659079b5c5985393e2a791f3de4fbbccd7
Azaruddin8297 <125579015+Azaruddin8297@users.noreply.github.com>
2023-03-23 23:29:55 +0530

    MultipleCompanies

--- Program.cs
+++ Program.cs
@@ -5,8 +5,12 @@
         public static void Main(string[] args)
         {
             EmpWageBulidArray empWageBulidArray = new EmpWageBulidArray();
-            empWageBulidArray.addCompanyEmpWages("DMart", 20, 2, 10);
+            empWageBulidArray.addCompanyEmpWages("DMart", 200, 20, 100);
+            empWageBulidArray.addCompanyEmpWages("Rliance", 300, 20, 120);
             empWageBulidArray.computeEmpWage();
+
+
         }
+    
     }
 }
