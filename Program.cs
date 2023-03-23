27616d6adc5cd00da829aff44441427c4180f116
Azaruddin8297 <125579015+Azaruddin8297@users.noreply.github.com>
2023-03-23 12:09:36 +0530

    add another company

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@
         {
             EmpWageBulidArray empWageBulidArray = new EmpWageBulidArray();
             empWageBulidArray.addCompanyEmpWages("DMart", 20, 2, 10);
+            empWageBulidArray.addCompanyEmpWages("Reliance", 40, 2, 20);
             empWageBulidArray.computeEmpWage();
 
 
