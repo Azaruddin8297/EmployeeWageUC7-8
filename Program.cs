c52fde06342b9cbdd9174f346c817d67ea68164e
Azaruddin8297 <125579015+Azaruddin8297@users.noreply.github.com>
2023-03-23 23:39:22 +0530

    Refactor

--- Program.cs
+++ Program.cs
@@ -4,10 +4,13 @@
     {
         public static void Main(string[] args)
         {
-            EmpWageBulidArray empWageBulidArray = new EmpWageBulidArray();
-            empWageBulidArray.addCompanyEmpWages("DMart", 200, 20, 100);
-            empWageBulidArray.addCompanyEmpWages("Rliance", 300, 20, 120);
-            empWageBulidArray.computeEmpWage();
+            //EmpWageBulidArray empWageBulidArray = new EmpWageBulidArray();
+            //empWageBulidArray.addCompanyEmpWages("DMart", 200, 20, 100);
+            //empWageBulidArray.addCompanyEmpWages("Rliance", 300, 20, 10);
+            //empWageBulidArray.computeEmpWage();
+            Console.WriteLine("Welcome To Employee Wage Computation Program.");
+            EmployeeWage.MonthlyEmployeeWage("Brand Factory", 50, 20, 130);
+            EmployeeWage.MonthlyEmployeeWage("Dell Technologies", 90, 20, 150);
 
 
         }
